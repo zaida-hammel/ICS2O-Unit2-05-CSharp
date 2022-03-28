83ece60fed891e17470f3def72b8d2fa1011aba5
zaida-hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-03-28 16:54:17 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,33 @@
+// Created by: Zaida Hammel
+// Created on: March 2022
+//
+// This program accepts user input
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function calculates the pay.
+        int Hours;
+        int Rate;
+
+        Console.WriteLine("This function calculates the pay.");
+        Console.WriteLine("");
+        Console.Write("Number of hours worked: ");
+        Hours = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Hourly rate: ");
+        Rate = Convert.ToInt32(Console.ReadLine());
+
+        Double Pay = (Hours * Rate) * (0.82);
+        Double Taxes = (Hours * Rate) *  (0.18);
+
+        Console.WriteLine("");
+        Console.Write("Your pay will be: " + Pay.ToString("0.00"));
+        Console.WriteLine("");
+        Console.Write("The government will take: " + Taxes.ToString("0.00"));
+        Console.ReadKey();
+        Console.WriteLine("\nDone.");
+    }
+}
